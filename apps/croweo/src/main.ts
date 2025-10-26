fb3fb60b51c11af2ca1a540ef0f68f51b4c49670
crowmagnumb <cromagnumb@pm.me>
2025-10-26 10:23:17 -0700

    have to fallback to what I know

--- apps/croweo/src/main.ts
+++ apps/croweo/src/main.ts
@@ -1,36 +1,8 @@
 import { NodeUtils } from "@animalus/node-core";
 import express from "express";
-import { CroweoConfig } from "@crowmagnumb/croweo-core";
+import { CroweoConfig, RandomMusic } from "@crowmagnumb/croweo-core";
 import pino from "pino";
 import path from "path";
-import { RandomUtils } from "@animalus/corejs";
-
-class RandomMusic {
-    running = false;
-
-    constructor(private rootDir: string, private files: string[]) {}
-
-    private next() {
-        const filename = path.join(this.rootDir, RandomUtils.nextItem(this.files))
-        console.log(`Playing ${filename}`);
-        NodeUtils.execSync(`play ${filename}`);
-        if (this.running) {
-            this.next();
-        }
-    }
-
-    start() {
-        this.running = true;    
-        this.next();    
-    }
-
-    stop() {
-        //
-        // TODO: Kill running process.
-        //
-        this.running = false;
-    }
-}
 
 // const configDir = __dirname;
 const configDir = "/opt/config/croweo"
